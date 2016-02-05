3ae3880e2e422fbe90bebe7d4cf9be3de120f9dc
Ubuntu <ubuntu@ip-172-31-29-106.us-west-2.compute.internal>
2016-02-05 17:47:25 +0000

    asdadg

--- src/main/scala/com/goguardian/http/spray/Boot.scala
+++ src/main/scala/com/goguardian/http/spray/Boot.scala
@@ -4,6 +4,7 @@ import akka.actor.{Actor, ActorSystem, Props}
 import akka.io.IO
 import spray.can.Http
 import spray.http.{HttpRequest, HttpResponse}
+import spray.http.HttpHeaders.Connection
 
 object Main extends App {
   implicit val system: ActorSystem = ActorSystem()
@@ -13,10 +14,10 @@ object Main extends App {
 class Boot extends Actor {
   import context.system
 
-  IO(Http) ! Http.Bind(self, interface = "0.0.0.0", port = 8080)
+  IO(Http) ! Http.Bind(self, interface = "0.0.0.0", port = 80)
 
   def receive = {
     case _: Http.Connected => sender ! Http.Register(self)
-    case _: HttpRequest => sender ! HttpResponse()
+    case _: HttpRequest => sender ! HttpResponse(headers = List(`Connection`("keep-alive")))
   }
 }
